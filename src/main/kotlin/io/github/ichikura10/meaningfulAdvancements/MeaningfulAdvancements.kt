9bb1c90ee0108742138f9ca84fd940058fac52de
ichikura10 <254886939+ichikura10@users.noreply.github.com>
2026-06-21 16:07:54 +0900

    fix: eventを感知できていなかった問題を修正

--- src/main/kotlin/io/github/ichikura10/meaningfulAdvancements/MeaningfulAdvancements.kt
+++ src/main/kotlin/io/github/ichikura10/meaningfulAdvancements/MeaningfulAdvancements.kt
@@ -1,21 +1,24 @@
 package io.github.ichikura10.meaningfulAdvancements
 
+import io.github.ichikura10.meaningfulAdvancements.event.ModEvents
 import net.minecraft.client.Minecraft
-import net.neoforged.bus.api.SubscribeEvent
-import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.fml.common.Mod
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent
 import net.neoforged.fml.event.lifecycle.FMLDedicatedServerSetupEvent
+import net.neoforged.neoforge.common.NeoForge
 import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 import thedarkcolour.kotlinforforge.neoforge.forge.runForDist
 
 @Mod(MeaningfulAdvancements.ID)
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 object MeaningfulAdvancements {
     const val ID = "meaningful_advancements"
 
     init {
+        MOD_BUS.addListener(::onCommonSetup)
+
+        NeoForge.EVENT_BUS.register(ModEvents)
+
         val obj = runForDist(clientTarget = {
             MOD_BUS.addListener(::onClientSetup)
             Minecraft.getInstance()
@@ -33,7 +36,6 @@ object MeaningfulAdvancements {
     private fun onServerSetup(event: FMLDedicatedServerSetupEvent) {
     }
 
-    @SubscribeEvent
     fun onCommonSetup(event: FMLCommonSetupEvent) {
     }
 }
--- src/main/kotlin/io/github/ichikura10/meaningfulAdvancements/event/ModEvents.kt
+++ src/main/kotlin/io/github/ichikura10/meaningfulAdvancements/event/ModEvents.kt
@@ -1,12 +1,12 @@
 package io.github.ichikura10.meaningfulAdvancements.event
 
-import io.github.ichikura10.meaningfulAdvancements.MeaningfulAdvancements
 import net.minecraft.world.entity.player.Player
-import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.neoforge.event.entity.player.AdvancementEvent
 
-@EventBusSubscriber(modid = MeaningfulAdvancements.ID, bus = EventBusSubscriber.Bus.GAME)
-class ModEvents {
+object ModEvents {
+
+    @SubscribeEvent
     fun onAdvancementEarn(event: AdvancementEvent.AdvancementEarnEvent) {
         val player: Player = event.entity
         println(player.name.string + "earned" + event.advancement.id)
